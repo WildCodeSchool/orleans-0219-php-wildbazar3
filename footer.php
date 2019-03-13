0707cb822311ac7a45bbd25b59c704c5bd9a3b40
Casey Heagerty <cheagerty@gmail.com>
2019-03-13 10:25:46 +0100

    footer.php added and reduced

---
+++ footer.php
@@ -0,0 +1,126 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet"> 
+
+    <link rel="stylesheet" href="style.css" />
+    
+
+    <title>Wild Bazar</title>
+  </head>
+
+
+
+
+  <body>
+
+
+
+
+  <!--Start of footer-->
+  <footer id="contact">
+    
+    <div class="container">
+      <div class="row">
+
+        <!--Start of left column-->
+        <div class="col-12 col-sm-6">
+          
+
+          <div class="row text-center"> <!--Start of row #2 of left column-->
+            <div class="col">
+              <img class="imgfooter" src="img/logo_wb.png" alt="Wild Bazar Logo">
+            </div>
+            <div class="col">
+              <h4 class="h4-footer">WILD&nbsp;BAZAR</h4>
+              <p class="address">1, rue de la paix<br>75000 Paris<br>+33&nbsp;(0)1&nbsp;23&nbsp;45&nbsp;67&nbsp;89</p>
+            </div> 
+          </div> <!--end of row #2 of left column-->
+
+          <div class="row categories_footer"> <!--Start of row #1 of left column-->
+           <div class="col"> 
+            <ul class="list-group">
+              <li class="list-group-item border-0">
+                <a href="#" class="a_footer">Home</a>
+              </li>
+              <li class="list-group-item border-0">
+                <a href="about" class="a_footer">About us</a>
+              </li>
+              <li class="list-group-item border-0">
+                <a href="topselling" class="a_footer">Top Selling</a>
+              </li>
+              <li class="list-group-item border-0">
+                <a href="contact" class="a_footer">Contact</a>
+              </li>
+              <li class="list-group-item border-0">
+                <a href="#" class="a_footer">Legal mentions</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col">
+            <ul class="list-group">
+              <li class="list-group-item border-0">
+                <a href="books.html" class="a_footer">Livres</a>
+              </li>
+              <li class="list-group-item border-0">
+                <a href="toys.html" class="a_footer">Jouets</a>
+              </li>
+              <li class="list-group-item border-0">
+                <a href="poubelle.html" class="a_footer">Poubelles</a>
+              </li>
+              <li class="list-group-item border-0">
+                <a href="usb.html" class="a_footer">Clés USB</a>
+              </li>
+            </ul>
+          </div>
+          </div> <!--End of row #1 of left column-->
+       
+        </div>
+        <!--end of left column-->
+
+        <!--Start of right column-->
+        <div class="col-12 col-sm-6">
+          <!--Start of Contact form-->
+          <h6 class="formheader">Contact us</h6>
+          <form>
+            <div class="form-group formlabel">
+              <label for="email">Email</label>
+              <input type="email" class="form-control" id="email" placeholder="@">
+            </div>
+            <div class="form-group formlabel">
+              <label for="text">Message</label>
+              <textarea class="form-control" id="text" rows="3" placeholder="Your message"></textarea>
+            </div>
+            <div class="submitbutton col-4 offset-5">
+              <button type="button" class="btn btn_footer">Submit</button>
+            </div>
+          </form>
+        </div>
+        <!--End of Contact form-->
+      <!--End of right column-->
+      </div>
+    </div>
+  </footer>
+  <!--End of footer-->
+
+
+
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
