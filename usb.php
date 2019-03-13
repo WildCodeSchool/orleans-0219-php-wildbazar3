088c424b5d5a3ac96f5ac90c1dbfc5c7e0dd8fce
Casey Heagerty <cheagerty@gmail.com>
2019-03-13 15:54:56 +0100

    usb.html to usb.php

--- usb.php
+++ usb.php
@@ -32,42 +32,7 @@
 
          <header>
 
-        <nav class="navbar navbar-expand-lg navbar-light sticky-top w-100">
-            
-          <a class="navbar-brand" href="#">
-            <img class="img_header" src="img/logo_wb.png">&nbsp;</a>
-        
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-
-            <span class="navbar-toggler-icon"></span>
-
-          </button>
-        
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav ml-auto">
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle a_header" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Catégorie
-                  </a>
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item a_header" href="books.html">Books</a>
-                    <a class="dropdown-item a_header" href="toys.html">Jouets</a>
-                    <a class="dropdown-item a_header" href="poubelles.html">Poubelles</a>
-                    <a class="dropdown-item a_header" href="usb.html">Clefs USB</a>
-                  </div>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link a_header" href="#about">About Us</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link a_header" href="#topselling">Top selling</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link a_header" href="#contact">Contact Us</a>
-                </li>
-              </ul>
-            </div>
-        </nav>
+      <?php include 'header.php'; ?>
 
       <!---------------------- Header -------------------------->
 
@@ -849,7 +814,7 @@
                 <a href="poubelle.html" class="a_footer">Poubelles</a>
               </li>
               <li class="list-group-item border-0">
-                <a href="usb.html" class="a_footer">Clés USB</a>
+                <a href="usb.php" class="a_footer">Clés USB</a>
               </li>
             </ul>
           </div>
