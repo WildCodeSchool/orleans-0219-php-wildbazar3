36525cddead8b78dca15d1edc98ef68eeef3ef44
Sylvain Aubert <sylvain@atelierdixdouze.com>
2019-03-14 17:48:22 +0100

    Ajout fichiers php pour modals et cards

--- books.php
+++ books.php
@@ -39,24 +39,24 @@
     	<div class="container">
       	<div class="row justify-content-center">
         	
-          <div class="col-md-6 col-lg-3 mx-auto card_margin">
-        		<div class="card card-border w-100 h-100">
-            	<img class="card-img-top" src="img/couv_bathroom.png" alt="Bathroom Manners">
-    						<div class="books_card_text align-middle">
-                	<p class="align-middle">26 €</p>
-                </div>
-              <div class="card-body">
-              	<h4 class="card-title"></h4>
-                <p class="card-text card_description"><em>Bathroom Manners</em><br/>
-                Textes de Jean-Claude&nbsp;Kauffman</p>
-              </div>
-              <div class="card-footer card-footer-border text-center">
-                <button type="button" class="btn btn_item" data-toggle="modal" data-target="#modal_1">
+            <div class="col-md-6 col-lg-3 mx-auto card_margin">
+        	    <div class="card card-border w-100 h-100">
+            	    <img class="card-img-top" src="img/couv_bathroom.png" alt="Bathroom Manners">
+                    <div class="books_card_text align-middle">
+                	    <p class="align-middle">26 €</p>
+                    </div>
+                    <div class="card-body">
+              	        <h4 class="card-title"></h4>
+                        <p class="card-text card_description"><em>Bathroom Manners</em><br/>
+                        Textes de Jean-Claude&nbsp;Kauffman</p>
+                    </div>
+                    <div class="card-footer card-footer-border text-center">
+                        <button type="button" class="btn btn_item" data-toggle="modal" data-target="#modal_1">
                 	Show more
-								</button>
-              </div>
-						</div>
-          </div>
+					    </button>
+                    </div>
+				</div>
+            </div>
           
           <div class="col-md-6 col-lg-3 mx-auto card_margin">
           	<div class="card card-border w-100 h-100">
---
+++ modal_cards/modalBooks.php
@@ -0,0 +1,34 @@
+<div class="modal fade" id="modal_1" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalCenterTitle">Bathroom Manners</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body w-100">
+                <div class="row ">
+                    <div class="col-sm-12 col-md-6 justify-content-center">
+                        <img src="../img/couv_bathroom.png">
+                    </div>
+                    <div class="col-sm-12 col-md-6 justify-content-center">
+                        <p class="p_descriptif_modal"><em>Bathroom Manners</em> réunit des photographies prises dans des salles de bains par des photographes inspirés.</p>
+                    </div>
+                </div>
+                <div class="row ">
+                    <div class="col">
+                        <p class="p_caract_modal"><span style="font-weight:700">Caractéristiques</span><br/>
+                            2004<br/>
+                            132 pages<br/>
+                            Broché, jaquette<br/>
+                            Éditions bilingue français-anglais</p>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn_item">Add to cart</button>
+            </div>
+        </div>
+    </div>
+</div>
