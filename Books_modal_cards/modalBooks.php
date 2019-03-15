a976ca5761cd2bb35d3bfc6b723937100a12bac7
Sylvain Aubert <sylvain@atelierdixdouze.com>
2019-03-15 13:36:06 +0100

    modification des fichiers pour ajout php

--- Books_modal_cards/modalBooks.php
+++ Books_modal_cards/modalBooks.php
@@ -1,8 +1,8 @@
-<div class="modal fade" id="modal_1" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+<div class="modal fade" id="<?php echo $book['dataTarget']; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalCenterTitle">Bathroom Manners</h5>
+                <h5 class="modal-title" id="exampleModalCenterTitle"><?php echo $book['bookTitle']; ?></h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -10,19 +10,19 @@
             <div class="modal-body w-100">
                 <div class="row ">
                     <div class="col-sm-12 col-md-6 justify-content-center">
-                        <img src="../img/couv_bathroom.png">
+                        <img src="<?php echo $book['image']; ?>">
                     </div>
                     <div class="col-sm-12 col-md-6 justify-content-center">
-                        <p class="p_descriptif_modal"><em>Bathroom Manners</em> réunit des photographies prises dans des salles de bains par des photographes inspirés.</p>
+                        <p class="p_descriptif_modal"><?php echo $book['bookresume']; ?></p>
                     </div>
                 </div>
                 <div class="row ">
                     <div class="col">
                         <p class="p_caract_modal"><span style="font-weight:700">Caractéristiques</span><br/>
-                            2004<br/>
-                            132 pages<br/>
-                            Broché, jaquette<br/>
-                            Éditions bilingue français-anglais</p>
+                            <?php echo $book['bookYear']; ?><br/>
+                            <?php echo $book['bookPage']; ?><br/>
+                            <?php echo $book['bookShaping']; ?><br/>
+                            <?php echo $book['bookSize']; ?></p>
                     </div>
                 </div>
             </div>
