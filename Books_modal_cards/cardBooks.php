ca4ef372f9197b14da186f9bb0abd8ff82299850
Sylvain Aubert <sylvain@atelierdixdouze.com>
2019-03-27 16:41:37 +0100

    Création PDO - Database sur partie 'Books'

--- Books_modal_cards/cardBooks.php
+++ Books_modal_cards/cardBooks.php
@@ -2,7 +2,7 @@
     <div class="card card-border w-100 h-100">
         <img class="card-img-top" src="<?php echo $book['image']; ?>" alt="Bathroom Manners">
         <div class="books_card_text align-middle">
-            <p class="align-middle"><?php echo $book['price']; ?></p>
+            <p class="align-middle"><?php echo $book['price'] . ' €'; ?></p>
         </div>
         <div class="card-body">
             <h4 class="card-title"></h4>
@@ -10,7 +10,8 @@
                 <?php echo $book['bookAuthor']; ?></p>
         </div>
         <div class="card-footer card-footer-border text-center">
-            <button type="button" class="btn btn_item" data-toggle="modal" data-target="<?php echo '#' . $book['dataTarget']; ?>">
+            <button type="button" class="btn btn_item" data-toggle="modal"
+                    data-target="<?php echo '#' . 'modal_' . $book['id']; ?>">
                 Show more
             </button>
         </div>
--- Books_modal_cards/modalBooks.php
+++ Books_modal_cards/modalBooks.php
@@ -1,4 +1,5 @@
-<div class="modal fade" id="<?php echo $book['dataTarget']; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+<div class="modal fade" id="<?php echo 'modal_' . $book['id']; ?>" tabindex="-1" role="dialog"
+     aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -13,16 +14,16 @@
                         <img src="<?php echo $book['image']; ?>">
                     </div>
                     <div class="col-sm-12 col-md-6 justify-content-center">
-                        <p class="p_descriptif_modal"><?php echo $book['bookresume']; ?></p>
+                        <p class="p_descriptif_modal"><?php echo $book['bookResume']; ?></p>
                     </div>
                 </div>
                 <div class="row ">
                     <div class="col">
                         <p class="p_caract_modal"><span style="font-weight:700">Caractéristiques</span><br/>
-                            <?php echo $book['bookYear']; ?><br/>
-                            <?php echo $book['bookPage']; ?><br/>
-                            <?php echo $book['bookShaping']; ?><br/>
-                            <?php echo $book['bookSize']; ?></p>
+                            <?= 'Paru en ' . $book['bookYear']; ?><br/>
+                            <?= $book['bookPage'] . ' pages'; ?><br/>
+                            <?= $book['bookShaping']; ?><br/>
+                            <?= $book['bookSize']; ?></p>
                     </div>
                 </div>
             </div>
